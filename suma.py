bed95c59fbbe15de9cbb847d928d1ca5dc7e1045
Jean263 <jpv.mora@yavirac.edu.ec>
2025-10-12 21:15:43 -0500

    prueba4

--- suma.py
+++ suma.py
@@ -5,7 +5,7 @@ numero2 = 3
 resultado = sumar(numero1, numero2)
 print(f"La suma de {numero1} y {numero2} es {resultado}")
 
-resultado_esperado = 8
+resultado_esperado = 9
 
 if resultado == resultado_esperado:
     print("La función sumar funciona correctamente")
